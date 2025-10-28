bb0d888fa1eab34b5dd4957f6cf288ac3457a241
Daniel Raffer <daniel.raffer@gmail.com>
2025-10-28 19:48:03 +0100

    Add extended function keys F13-F24 to Keycode map

--- keycode.go
+++ keycode.go
@@ -20,3 +20,28 @@ var Keycode = keycode.Keycode
 
 // Special defines the special key map
 var Special = keycode.Special
+
+// init function to add extended function keys
+func init() {
+	// Add extended function keys F13-F24
+	// These are common keycodes for extended function keys on Windows
+	extendedFKeys := map[string]uint16{
+		"f13": 124, // F13
+		"f14": 125, // F14
+		"f15": 126, // F15
+		"f16": 127, // F16
+		"f17": 128, // F17
+		"f18": 129, // F18
+		"f19": 130, // F19
+		"f20": 131, // F20
+		"f21": 132, // F21
+		"f22": 133, // F22
+		"f23": 134, // F23
+		"f24": 135, // F24
+	}
+	
+	// Add the extended function keys to the Keycode map
+	for key, code := range extendedFKeys {
+		Keycode[key] = code
+	}
+}
